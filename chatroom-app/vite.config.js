39a8ba64a83c74033b085a72b79cd3ddbbfcbcd3
Yavanna Perez <yavannaperez@hotmail.com>
2024-05-06 09:00:54 +0200

    test again url base githubpages

--- chatroom-app/vite.config.js
+++ chatroom-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lokkeroom-project/chatroom-front-end-app/',
+  // base: '/lokkeroom-project/chatroom-front-end-app/',
+  base: '/chatroom-front-end-app/',
   plugins: [react()],
 })
